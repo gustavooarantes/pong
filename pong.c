feed535e4431e475f0446524abb1e2bed83307f6
gustavooarantes <dev.gustavoa@gmail.com>
2025-12-19 14:43:01 -0300

    fix: fixed error handling for creating window and renderer

--- pong.c
+++ pong.c
@@ -16,13 +16,19 @@ int main() {
 		SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 						 640, 480, SDL_WINDOW_SHOWN);
 
-	SDL_Renderer *renderer = SDL_CreateRenderer(pwindow, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
 	if (!pwindow) {
-		SDL_Log("SDL_CreateWindow errro: %s", SDL_GetError());
+		SDL_Log("SDL_CreateWindow error: %s", SDL_GetError());
+		SDL_Quit();
+		return 1;
+	}
+
+	SDL_Renderer *prenderer = SDL_CreateRenderer(pwindow, -1, 0);
+	SDL_SetRenderDrawColor(prenderer, 0, 0, 0, 255);
+	SDL_RenderClear(prenderer);
+	SDL_RenderPresent(prenderer);
+
+	if (!prenderer) {
+		SDL_Log("SDL_CreateRenderer error: %s", SDL_GetError());
 		SDL_Quit();
 		return 1;
 	}
